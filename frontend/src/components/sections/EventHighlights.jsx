5498005972cf774c585b356e207940afbfb549d7
Arewa Tolulope <tolulopearewa@student.oauife.edu.ng>
2026-02-01 15:19:26 +0100

    changed the subject

--- frontend/src/components/sections/EventHighlights.jsx
+++ frontend/src/components/sections/EventHighlights.jsx
@@ -44,7 +44,8 @@ const EventHighlights = () => {
               <a href="https://luma.com/f3fpygbt" target="_blank" rel="noopener noreferrer">
                 <button className="mt-4 bg-[#FDB913] text-black px-4 py-2 rounded-lg font-semibold hover:bg-[#ddcb9d] w-full">Preregister Now</button>
               </a>
-              <a href="https://mail.google.com/mail/?view=cm&fs=1&to=partnership@coengagex.com" target="_blank" rel="noopener noreferrer">
+              <a  href="https://mail.google.com/mail/?view=cm&fs=1&to=partnership@coengagex.com&su=Partnership Inquiry&body=Hello CoEngageX,%0D%0AI am interested in partnering with you."
+                target="_blank" rel="noopener noreferrer">
                 <button className="mt-4 text-[#FDB913] bg-black hover:bg-gray-700 hover:text-white px-4 py-2 rounded-lg font-semibold w-full">Request Sponsorsip Desk</button>
               </a>
             </div>
